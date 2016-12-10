9a6666c7a836466f9003d11d90ac053b0b1f43c8
Ajinkya Kher <ajkher@microsoft.com>
2016-12-09 19:46:08 -0800

    Reverting back the redirect_uri in the intermin until a valid long term fix with the shortened uri is found

--- Skype/WebSDK/index.js
+++ Skype/WebSDK/index.js
@@ -794,7 +794,7 @@
                         client_id: client_id,
                         origins: ["https://webdir.online.lync.com/autodiscover/autodiscoverservice.svc/root"],
                         cors: true,
-                        redirect_uri: location.origin + location_config.content + 'token.html'
+                        redirect_uri: location.href + 'websdk/token.html'
                     }).then(function () {
                         document.getElementsByClassName('before-signin')[0].style.display = 'none';
                         document.getElementsByClassName('azuread-signin')[0].style.display = 'none';
