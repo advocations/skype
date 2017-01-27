ad8db9f3bcd0b944ddc00df83e22ee8f45bffbdf
Seth Lifland <sethli@microsoft.com>
2017-01-26 18:06:01 -0800

    move guid function to a shared location

--- Skype/WebSDK/samples/Meetings/Anonymous Online/index.ts
+++ Skype/WebSDK/samples/Meetings/Anonymous Online/index.ts
@@ -234,16 +234,6 @@
     window.framework.addEventListener(content.querySelector('.restart'), 'click', restart);
     window.framework.addEventListener(content.querySelector('.getToken'), 'click', getToken);
 
-    function guid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
-    }
-
     function getToken() {
         window.framework.showNotificationBar();
         if (!(<HTMLInputElement>content.querySelector('.meeting_url')).value) {
@@ -276,7 +266,7 @@
             }
         };
 
-        var data = "ApplicationSessionId=" + guid() +
+        var data = "ApplicationSessionId=" + window.framework.utils.guid() +
             "&AllowedOrigins=" + encodeURIComponent(allowedOrigins) +
             "&MeetingUrl=" + encodeURIComponent(meetingUrl);
 
