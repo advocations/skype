6394c782df3f2e7a20f823bfac00fa85f8a462f2
Sandeep Kumar (SKYPE) <sankum@microsoft.com>
2017-02-15 15:48:50 -0800

    Add Trusted Application API QuickStartSamples

---
+++ Skype/Trusted-Application-API/samples/QuickStartSamples/QuickSamplesCommon/Configs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSamplesCommon
+{
+    public class QuickSamplesConfig
+    {
+        public static string ApplicationEndpointId { get; private set; }
+        public static string AAD_ClientId  { get; private set; }
+        public static string AAD_ClientSecret  { get; private set; }
+
+        static QuickSamplesConfig()
+        {
+            ApplicationEndpointId = ConfigurationManager.AppSettings["ApplicationEndpointId"];
+            AAD_ClientId = ConfigurationManager.AppSettings["AAD_ClientId"];
+            AAD_ClientSecret = ConfigurationManager.AppSettings["AAD_ClientSecret"];
+        }
+    }
+}
---
+++ Skype/Trusted-Application-API/samples/QuickStartSamples/QuickSamplesCommon/ConsoleLogger.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.SfB.PlatformService.SDK.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSamplesCommon
+{
+    public class ConsoleLogger : IPlatformServiceLogger
+    {
+        public bool HttpRequestResponseNeedsToBeLogged
+        {
+            get;
+            set;           
+        }
+
+        public void Information(string message)
+        {
+            Console.WriteLine("[INFO]" + message);
+        }
+
+        public void Information(string fmt, params object[] vars)
+        {          
+            Console.WriteLine("[INFO]" + string.Format(fmt, vars));
+        }
+
+        public void Information(Exception exception, string fmt, params object[] vars)
+        {
+            string msg = String.Format(fmt, vars);
+            Console.WriteLine("[INFO]" + msg + "; \r\nException Details= ", ExceptionUtils.FormatException(exception, includeContext: true));
+        }
+
+        //
+        // Warning - trace warnings within the application
+
+        public void Warning(string message)
+        {
+            Console.WriteLine("[WARN]" + message);
+        }
+
+        public void Warning(string fmt, params object[] vars)
+        {
+            Console.WriteLine("[WARN]" + string.Format(fmt, vars));
+        }
+
+        public void Warning(Exception exception, string fmt, params object[] vars)
+        {
+            string msg = String.Format(fmt, vars);
+            Console.WriteLine(msg + "; \r\nException Details= ", ExceptionUtils.FormatException(exception, includeContext: true));
+        }
+
+        //
+        // Error - trace fatal errors within the application
+
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error(string fmt, params object[] vars)
+        {
+            Console.WriteLine("[ERROR]" + String.Format(fmt, vars));
+        }
+
+        public void Error(Exception exception, string fmt, params object[] vars)
+        {
+            string msg = String.Format(fmt, vars);
+            Console.WriteLine("[ERROR]" + msg + "; \r\nException Details= ", ExceptionUtils.FormatException(exception, includeContext: true));
+        }
+
+    }
+}
