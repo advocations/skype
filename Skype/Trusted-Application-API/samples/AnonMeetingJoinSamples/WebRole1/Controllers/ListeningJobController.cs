bb7a1c91d4ae2ae1649129089d43c0ed15abb624
Sandeep Kumar (SKYPE) <sankum@microsoft.com>
2017-02-15 16:04:47 -0800

    Add Trusted Application API AnonMeetingJoinSamples

---
+++ Skype/Trusted-Application-API/samples/AnonMeetingJoinSamples/WebRole1/Controllers/ListeningJobController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.SfB.PlatformService.SDK.Samples.ApplicationCore;
+
+namespace Microsoft.SfB.PlatformService.SDK.Samples.FrontEnd
+{
+    /// <summary>
+    /// Controller to handle jobs which are currently listening for incoming invitations.
+    /// </summary>
+    /// <remarks>At one point of time, we can have only one job listening.</remarks>
+    [MyCorsPolicy]
+    public class ListeningJobController : JobControllerBase
+    {
+        /// <summary>
+        /// Get the currently running job.
+        /// </summary>
+        /// <returns>Currently running job details or a <code>404</code>, if no job is currently listening.</returns>
+        public HttpResponseMessage Get()
+        {
+            var listeningJob = PlatformServiceListeningJobBase.ListeningJob;
+            if (listeningJob == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "{\"Error\":\"No listening job present at the moment\"}");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, listeningJob);
+        }
+
+        /// <summary>
+        /// Stops the currently running job.
+        /// </summary>
+        /// <returns><code>204</code> in case of success or a <code>404</code>, if no job is currently listening.</returns>
+        public HttpResponseMessage Delete()
+        {
+            var listeningJob = PlatformServiceListeningJobBase.ListeningJob;
+            if (listeningJob == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "{\"Error\":\"No listening job present at the moment\"}");
+            }
+
+            listeningJob.Stop();
+
+            return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+    }
+}
