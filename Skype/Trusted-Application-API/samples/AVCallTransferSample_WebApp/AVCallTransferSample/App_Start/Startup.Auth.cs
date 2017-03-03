c9323af805c67f8d5807e5e7b2b0d77f518b87ba
JIE CHEN <jiche@microsoft.com>
2017-03-03 15:28:32 -0800

    Add AVCallTransfer sample

---
+++ Skype/Trusted-Application-API/samples/AVCallTransferSample_WebApp/AVCallTransferSample/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Owin;
+using System.Configuration;
+using Microsoft.Owin.Security.ActiveDirectory;
+using System.IdentityModel.Tokens;
+
+namespace CallCenterSample
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            string audienceUri = ConfigurationManager.AppSettings["MyAudienceUri"];
+            string endpointId = ConfigurationManager.AppSettings["ApplicationEndpointId"];
+            string tenant = endpointId.Split('@')[1];
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+            {
+                //If tenant is assigned  and ValidateIssuer = true, only this tenant is allowed to pass through
+                //If tenant is assigned "common",   ValidateIssuer = true, multitenant allowed
+                Tenant = tenant,         
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidAudiences = new List<string> { audienceUri }                     
+                }
+            });
+        }
+    }
+}
